7268cd8a43478e07a5e995c9a6409f875d9644f5
Igiraneza1 <134881953+Igiraneza1@users.noreply.github.com>
2025-03-25 14:22:44 +0200

    commit

---
+++ playground.js
@@ -0,0 +1,19 @@
+// calculating age
+
+let username = "Igiraneza Aime";
+const birthyear= 2000;
+const currentyear= 2025;
+
+let age= currentyear - birthyear; 
+
+console.log(`my name is${username} and i'm ${age}`);
+
+// calculating birth year
+
+const calBirthYear = (age) => currentyear-age  ;{
+    console.log(`My birth year is  ${calBirthYear(25)}`);
+}
+
+
+    
+
